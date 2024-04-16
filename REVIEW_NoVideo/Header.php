fe9ca14030a5ca9f1f51b31ccd274596c645610e
ZyonR <2022014951@feu.edu.ph>
2024-04-16 15:50:23 +0800

    Header.php

---
+++ REVIEW_NoVideo/Header.php
@@ -0,0 +1,7 @@
+<header class="header">
+    <div class="top-section">
+        <h1 class="title">AMADEUS</h1>
+        <a href="booking.php" class="header-button">Book Now</a>
+    </div>
+    <?php include'Navigation.php'; ?>
+</header>
